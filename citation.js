52146343a037fb8556a28d3897427b338b9598e6
Petar Mihaylov <petarmihaylov@users.noreply.github.com>
2024-03-08 08:17:56 -0500

    Create citation.js

---
+++ citation.js
@@ -0,0 +1,26 @@
+<script>
+$(document).ready(function() {
+    // Add Attribution Text
+    var attributionText = $('<span>', { 
+        text: ' | ', 
+        css: { 
+            // Style the link as needed 
+        }
+    }); 
+
+    // Dynamically get the base URL from the DOM
+    var baseURL = window.location.hostname;
+
+    // Add attribution link with UTM parameters, using the dynamically obtained base URL
+    var attributionLink = $('<a>', { 
+        href: 'https://retoolkit.io/?utm_source=' + encodeURIComponent(baseURL), 
+        text: 'REToolkit - SEO for Real Estate Investing ', 
+        css: { 
+            // Style the link as needed 
+        }
+    }); 
+
+    // Append the text and link to the element in the footer with the class "copy"
+    $('footer .copy').append(attributionText).append(attributionLink);
+});
+</script>
